2ff2ad2a86b1f5351cdb6b953646fb0c8b0e4217
Tiptev <aleksey.tiptev@gmail.com>
2021-10-10 20:04:56 +0500

    Итоговый вариант Lab4.code
    Были добавлены пояснения, и цикл для вывода выходных данных для проверки

--- Lab4.code.cpp
+++ Lab4.code.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
- int N;
- int price[2][1000];
- int way_distance[2][1000];
- int addit_array[3][1000];
+ int N;//кол-ва сотрудников
+ int price[2][1000];//первая строка массива - номер такси, вторая строка массива - цена за километр(тариф)
+ int way_distance[2][1000];//первая строка массива - номер сотрудника, вторая строка массива - расстояние (в км) от работы до дома
+ int addit_array[3][1000];//дополнительный массив для вывода информации: 1 ст. - номер сотрудника; 2 ст. - номер такси; 3 ст. - итоговая цена
 
 using namespace std;
 
-int sorting_dist(int way_distance[2][1000], int N)
+int sorting_dist(int way_distance[2][1000], int N)//сортировка массива методом пузырька
 {
     for (int s = 0; s < N-1; s++){
         for (int i = 0; i < N-1; i++){
@@ -20,7 +20,7 @@ int sorting_dist(int way_distance[2][1000], int N)
   return 0;
 }
 
-int sorting_price(int price[2][1000], int N)
+int sorting_price(int price[2][1000], int N)//сортировка массива методом пузырька
 {
     for (int s = 0; s < N-1; s++){
         for (int i = 0; i < N-1; i++){
@@ -34,39 +34,39 @@ int sorting_price(int price[2][1000], int N)
   return 0;
 }
 
-int data_entry_dist(int way_distance[2][1000],int N)
+int data_entry_dist(int way_distance[2][1000],int N)//заполнения массива информацией
 {
     for (int i = 0; i < N; i++)
         {
            cout << "Введите расстояние сотрудника № " << i+1<<" от места работы до дома  - ";
-           cin >> way_distance[2][i];   
-           way_distance[1][i] = i + 1;
+           cin >> way_distance[2][i]; // расстояние от работы до дома   
+           way_distance[1][i] = i + 1;//номер сотрудника
         }
         
     return 0;
 }
 
-int data_entry_price(int price[2][1000],int N)
+int data_entry_price(int price[2][1000],int N)//заполнения массива информацией
 {
     for (int i = 0; i < N; i++)
         {
            cout << "Введите тариф за проезд одного километра в такси №" << i+1<<" - ";
-           cin >> price[2][i];
-           price[1][i] = i + 1; 
+           cin >> price[2][i];//тариф таксиста
+           price[1][i] = i + 1;//номер такси
         }
     return 0;
 }
 
 int price_calcul(int price[2][1000],int way_distance[2][1000],int addit_array[3][1000],int N)
 {
-    for (int f = 0; f < N; f++)
+    for (int f = 0; f < N; f++)//цикл для присвоения значений в массив addit_array
         {
             addit_array[3][f]=way_distance[2][f]*price[2][f];
             addit_array[2][f]=price[1][f];
             addit_array[1][f]=way_distance[1][f];
         }
         
-    for(int s = 0; s < N-1; s++)
+    for(int s = 0; s < N-1; s++)//сортировка методом пузырька, для востоновления порядка сотрудников
         {
             for (int i = 0; i < N-1; i++)
                 {
@@ -81,6 +81,9 @@ int price_calcul(int price[2][1000],int way_distance[2][1000],int addit_array[3]
         
     for (int d = 0; d < N; d++)
         cout << "Сотрудник №" << addit_array[1][d] << " сядет в такси №" << addit_array[2][d] << " и стоимость проезда(в рублях) будет равна = " << addit_array[3][d]<<"."<<endl;
+        cout<<"Выходные данные для проверки(номера такси):  ";
+    for (int h = 0; h < N; h++)
+        cout<<addit_array[2][h]<<" , ";//вывод номеров такси
     return 0;  
 }
 
@@ -93,6 +96,5 @@ int main()
     data_entry_price(price, N);    
     sorting_price(price ,N); 
     price_calcul(price, way_distance, addit_array, N);
-    
     return 0;
 }
